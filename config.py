16890a7ec6a5939a7268f659c5e73f75d574f6f7
HL <rlimm2@knou.ac.kr>
2024-09-20 00:34:05 +0900

    url수정

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'postgresql://default:Hw6ayU7DYRKm@ep-orange-king-a43tjzt8-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'postgres://default:Hw6ayU7DYRKm@ep-orange-king-a43tjzt8-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # or another method to set a secret key
