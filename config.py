4f940ddb8f2444ecedd327a22a4645a68b7773f9
HL <rlimm2@knou.ac.kr>
2024-09-20 13:53:24 +0900

    Database_URL modified

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # or another method to set a secret key
